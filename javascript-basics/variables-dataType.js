32c6ad09ef5bf084d737d1ce52f0ca4a0dbfb3c2
Ashutosh Kumar Singh <ashutoshsingh98614@gmail.com>
2026-08-10 23:08:15 +0530

    practiced variables with javascript.info exercises

---
+++ javascript-basics/variables-dataType.js
@@ -0,0 +1,54 @@
+// A variable is a 'namestorage' for data.
+// let message;
+// here 'let' is a keyword and 'message' create/declare variable with the name 'message'.
+
+// Task - 1
+// Working with variables
+// 1.Declare two variables: admin and name.
+// 2.Assign the value "John" to name.
+// 3.Copy the value from name to admin.
+// 4.Show the value of admin using alert (must output “John”).
+// Solution:-
+// let admin;
+// let name = "John";
+// admin = name;
+// alert(admin);
+
+// Task - 2
+// 1.Create a variable with the name of our planet. How would you name such a variable?
+// let ourPlanetName = "Earth";
+// 2.Create a variable to store the name of a current visitor to a website. How would you name that variable?
+// let currentUserName = "Ashutosh";
+
+// Task - 3
+// 5 variables banao — kuch let se, kuch const se — jisme string, number, boolean, aur ek array store karo
+// let name = "Ashutosh";
+// let age = 25;
+// let isAdult = true;
+// let fruit = ['Apple', 'Mango', 'Banana'];
+// const dob = '14-07-2001';
+// const pi = 3.14;
+
+// Task - 4
+// 'const' se banaya variable reassign karne ki koshish karo — error dekho, samjho kyun aata hai
+// const amount = 550;
+// amount = 570;
+// console.log(amount);
+// This gives error "TypeError: Assignment to constant variable."
+
+// Task - 5
+// same naam se let variable do baar declare karne ki koshish karo — kya hota hai dekho
+// let amount = 550;
+// let amount = 570;
+// console.log(amount);
+// This gives error "SyntaxError: Identifier 'amount' has already been declared"
+
+// Task - 6
+// 3 variables banao let se — apna naam, age, aur ek boolean (isEmployed)
+// let name = "Ashutosh";
+// let age = 25;
+// let isEmployed = true;
+
+// Task - 7
+// 1 variable const se banao — koi fix value (jaise PI = 3.14)
+// const dob = '14-07-2026';
