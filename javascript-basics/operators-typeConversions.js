7e19ee34bfc688390f664d914010acb759dd9744
Ashutosh Kumar Singh <ashutoshsingh98614@gmail.com>
2026-08-12 22:57:21 +0530

    Learn and Practice about Operator in JavaScript.

---
+++ javascript-basics/operators-typeConversions.js
@@ -0,0 +1,31 @@
+// Operand:- An operand is what operators are applied. In Multiplication of 5 * 2, left operand is '5' and the right operand is '2'. Some times we call them 'argument' instead of 'Operands'.
+
+// Unary:- An operator is unary if it has a single operand.
+// let x = 5;
+// x =  -x;
+// console.log(x); output will be -5.
+
+// Binary:- An operator is binary if it has two operand. 
+// let a = 4;
+// let b = 9;
+// console.log(b-a); Output will be 5.
+
+// Arithmatic Operator
+// Addition +,
+// Subtraction -,
+// Multiplication *,
+// Division /,
+// Remainder %,
+// Exponentiation **.
+
+// Remainder %:-
+// The result of a % b is the reminder of the integer division of a by b.
+// console.log(5 % 2); reminder:- 1
+// console.log(8 % 3); reminder:- 2
+
+// Exponentiation **:-
+// The exponentition operator a**b raises a to the power of b
+// console.log(2**2); output:- 4
+// console.log(3**2); output:- 9
+// for non-integer number as well
+// console.log(4**1/2);   power of 1/2 is the same as a square root
