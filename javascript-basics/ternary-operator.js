d8ebdf18a0bb982b07097879cb20c51d368912ba
Ashutosh Kumar Singh <ashutoshsingh98614@gmail.com>
2026-08-19 07:34:41 +0530

    learn what is tarnary operator and why it is called ternary.

---
+++ javascript-basics/ternary-operator.js
@@ -0,0 +1,30 @@
+// Ternary Operator
+// Now let's say:
+
+// let age = 20;
+// if(age >= 18){
+//     console.log("Adult");
+// }
+// else{
+//     console.log("Minor");
+// }
+// This is perfectly Fine
+// but this is a very small decision.
+// JS gives us a shorter syntax called the Ternary Operator.
+
+// SYNTAX:
+// condition ? true_value : false_value
+
+// Example:
+// let age = 18;
+// let result = age >= 18 ? "Adult" : "Minor";
+// console.log(result);  Output: Adult
+
+// How to read it: age >= 18 ? "Adult" : "Minor";
+// Is age greater than and equal to 18 ?
+// If yes: Adult
+// If no: Minor
+
+// Why it is called ternary?
+// Because it has three parts:
+// 1. condition  2. condition when true  3. condition when false
