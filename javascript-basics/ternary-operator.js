e0f85c7d2f4abae03325deb15adfa458e31370c0
Ashutosh Kumar Singh <ashutoshsingh98614@gmail.com>
2026-08-19 07:45:02 +0530

    learn a very important interview question for ternary operator

--- javascript-basics/ternary-operator.js
+++ javascript-basics/ternary-operator.js
@@ -28,3 +28,23 @@
 // Why it is called ternary?
 // Because it has three parts:
 // 1. condition  2. condition when true  3. condition when false
+
+// Very common interview question
+// why it is called a ternary operator?
+// Because itstakes three operands: a condition, a value for the true case, a value for the false case.
+
+// Ternary vs if/else
+// let age = 10;
+// let message;
+// if(age >= 18){
+//     message = "Adult";
+// }
+// else{
+//     message = "Minor"
+// }
+// console.log(message);
+
+// let age = 8;
+// let message = age >= 18 ? "Adult" : "Minor";
+// console.log(message);
+// Both produce the same result.
