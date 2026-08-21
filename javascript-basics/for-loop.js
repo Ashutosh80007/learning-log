9b4a2222b2f1ba870d8a1a2ad2a6b6ff85d5e546
Ashutosh Kumar Singh <ashutoshsingh98614@gmail.com>
2026-08-21 22:43:49 +0530

    Learn and practice for loop

---
+++ javascript-basics/for-loop.js
@@ -0,0 +1,31 @@
+// What is loop?
+// A loop is used when we want to execute the same code multiple times.
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+
+// for(i=1; i <= 5; i++){
+//     console.log(i);
+// }   
+// output: 1
+//         2
+//         3
+//         4
+//         5   A loop repeat a blocks of code until the condition false or until the loop is stopped.
+
+// For loop:
+// The most important SYNTAX:-
+// for(intialization; consition; updation){
+//     code
+// }
+// example:- 
+// for(i=0; i < 5; i++){
+//     console.log(i);
+// }   
+// output: 0
+//         1
+//         2
+//         3
+//         4  
